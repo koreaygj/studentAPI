aa330cd772bc10831b0173c2be59c713f192fe0c
koreaygj <ygj1828@gmail.com>
2023-12-13 15:30:37 +0900

    start:spring project

---
+++ src/main/java/goorm/strudentAPI/StrudentApiApplication.java
@@ -0,0 +1,13 @@
+package goorm.strudentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrudentApiApplication.class, args);
+	}
+
+}
